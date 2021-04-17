bc5d04270248cbe2c708c882f12793a1977781e1
Aaron Gershman <aegershman@gmail.com>
2021-04-17 00:51:48 -0500

    configure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
-	id("org.springframework.boot") version "2.4.5"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	id("java")
+    id("org.springframework.boot") version "2.4.5"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("java")
 
     id("nebula.release") version "15.3.1"
 
-	id("nebula.info") version "9.3.0"
+    id("nebula.info") version "9.3.0"
     id("nebula.javadoc-jar") version "17.3.2"
     id("nebula.source-jar") version "17.3.2"
     id("nebula.maven-apache-license") version "17.3.2"
@@ -17,15 +17,15 @@ group = "io.slugstack.oss"
 description = "for random testing with gradle-based projects"
 
 repositories {
-	mavenCentral()
-	maven {
-		url = uri("https://oss.sonatype.org/content/repositories/snapshots")
-	}
+    mavenCentral()
+    maven {
+        url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+    }
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 configure<nebula.plugin.release.git.base.ReleasePluginExtension> {
@@ -46,4 +46,8 @@ tasks.named<JavaCompile>("compileJava") {
 tasks.named<Test>("test") {
     useJUnitPlatform()
     jvmArgs = listOf("-XX:+UnlockDiagnosticVMOptions", "-XX:+ShowHiddenFrames")
+}
+
+configure<org.openrewrite.gradle.RewriteExtension> {
+    activeRecipe("org.openrewrite.java.format.AutoFormat")
 }
