3be13f2004cfeb7d06354ad4b4d39e9fd29f4a75
Aaron Gershman <aegershman@gmail.com>
2021-04-17 00:36:57 -0500

    scaffolding

---
+++ src/main/java/io/slugstack/oss/unspecifiedgradleproject/UnspecifiedGradleProjectApplication.java
@@ -0,0 +1,13 @@
+package io.slugstack.oss.unspecifiedgradleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnspecifiedGradleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnspecifiedGradleProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/slugstack/oss/unspecifiedgradleproject/UnspecifiedGradleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.slugstack.oss.unspecifiedgradleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnspecifiedGradleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
