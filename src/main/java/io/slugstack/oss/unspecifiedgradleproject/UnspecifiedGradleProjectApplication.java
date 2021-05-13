1566737fa30f8272d75fbdc1db3a068e65073baa
Aaron Gershman <aegershman@gmail.com>
2021-05-13 13:28:51 -0500

    again

--- src/main/java/io/slugstack/oss/unspecifiedgradleproject/UnspecifiedGradleProjectApplication.java
+++ src/main/java/io/slugstack/oss/unspecifiedgradleproject/UnspecifiedGradleProjectApplication.java
@@ -3,7 +3,7 @@ package io.slugstack.oss.unspecifiedgradleproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// @lombok.Data
+@lombok.Data
 // @java.lang.SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 // @SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 @SpringBootApplication
