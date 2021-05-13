5a24337cd47fc5c7d1663d42849bc6cea3b5dae4
Aaron Gershman <aegershman@gmail.com>
2021-05-13 13:34:23 -0500

    some reason for content

--- src/main/java/io/slugstack/oss/unspecifiedgradleproject/UnspecifiedGradleProjectApplication.java
+++ src/main/java/io/slugstack/oss/unspecifiedgradleproject/UnspecifiedGradleProjectApplication.java
@@ -1,5 +1,12 @@
 package io.slugstack.oss.unspecifiedgradleproject;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,4 +20,16 @@ public class UnspecifiedGradleProjectApplication {
         SpringApplication.run(UnspecifiedGradleProjectApplication.class, args);
     }
 
+}
+
+class Example {
+    // just wanting some reason to have imports and file content
+    void example() {
+        Set<String> someSet = new HashSet<>();
+        someSet.add("someExample");
+        Collection<String> someList = someSet.stream().collect(Collectors.toList());
+        someList.add("anotherExample");
+        List<String> emptyList = Collections.emptyList();
+        emptyList.size();
+    }
 }
