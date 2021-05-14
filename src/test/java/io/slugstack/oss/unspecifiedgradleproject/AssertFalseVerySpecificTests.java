11f8697f1019a03b93c8c15816acc87d1e9c883d
Aaron Gershman <aegershman@gmail.com>
2021-05-14 16:37:22 -0500

    very specific

---
+++ src/test/java/io/slugstack/oss/unspecifiedgradleproject/AssertFalseVerySpecificTests.java
@@ -0,0 +1,18 @@
+package io.slugstack.oss.unspecifiedgradleproject;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+
+public class AssertFalseVerySpecificTests {
+
+    @Test
+    public void testFalseWithMethodCall() {
+        assertFalse(isFalse());
+    }
+
+    private boolean isFalse() {
+        return false;
+    }
+
+}
