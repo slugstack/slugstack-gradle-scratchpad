0babb586c7d049f7c4b05b3612e0fd4a9a41f0ff
Aaron Gershman <aegershman@gmail.com>
2021-05-14 15:51:47 -0500

    test

--- src/test/java/io/slugstack/oss/unspecifiedgradleproject/AssertEqualsTests.java
+++ src/test/java/io/slugstack/oss/unspecifiedgradleproject/AssertEqualsTests.java
@@ -7,11 +7,20 @@ import org.junit.jupiter.api.Test;
 public class AssertEqualsTests {
 
     @Test
+    void testEqualsWithPrimativeInts() {
+        assertEquals(someInt(), someInt());
+    }
+
+    @Test
     void testEquals() {
         assertEquals(1, notification());
     }
 
     private Integer notification() {
+        return 1;
+    }
+
+    private int someInt() {
         return 1;
     }
 
