dc0d79436e13f05ebe4170d9d853ab74678a5cbb
Aaron Gershman <aegershman@gmail.com>
2021-05-14 15:02:20 -0500

    format

--- src/test/java/io/slugstack/oss/unspecifiedgradleproject/ExampleTests.java
+++ src/test/java/io/slugstack/oss/unspecifiedgradleproject/ExampleTests.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-class ExampleTests {
+public class ExampleTests {
 
     @Test
     void test() {
