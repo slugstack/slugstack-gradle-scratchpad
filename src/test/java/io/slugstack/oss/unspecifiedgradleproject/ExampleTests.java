ec52c62b0b3b8718de9b6d8c84bee003d66d49a3
Aaron Gershman <aegershman@gmail.com>
2021-05-14 15:37:41 -0500

    assert false

--- src/test/java/io/slugstack/oss/unspecifiedgradleproject/ExampleTests.java
+++ src/test/java/io/slugstack/oss/unspecifiedgradleproject/ExampleTests.java
@@ -1,14 +1,20 @@
 package io.slugstack.oss.unspecifiedgradleproject;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
 
 public class ExampleTests {
 
     @Test
-    void test() {
+    void testEquals() {
         assertEquals(1, notification());
+    }
+
+    @Test
+    public void testFalse() {
+        assertFalse(notification() != null && notification() > 0);
     }
 
     private Integer notification() {
