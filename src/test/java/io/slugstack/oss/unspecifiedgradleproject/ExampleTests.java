fc6fe3cf3dd076daf3a3eba0c9c8b4004fd287bd
Aaron Gershman <aegershman@gmail.com>
2021-05-14 14:38:02 -0500

    example tests

---
+++ src/test/java/io/slugstack/oss/unspecifiedgradleproject/ExampleTests.java
@@ -0,0 +1,18 @@
+package io.slugstack.oss.unspecifiedgradleproject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class ExampleTests {
+
+    @Test
+    void test() {
+        assertEquals(1, notification());
+    }
+
+    private Integer notification() {
+        return 1;
+    }
+
+}
