2711d1726fba300f68ed91bcb991c0a2f6c6dd67
Aaron Gershman <aegershman@gmail.com>
2021-05-14 15:49:34 -0500

    renaming

--- src/test/java/io/slugstack/oss/unspecifiedgradleproject/AssertEqualsTests.java
+++ src/test/java/io/slugstack/oss/unspecifiedgradleproject/AssertEqualsTests.java
@@ -1,20 +1,14 @@
 package io.slugstack.oss.unspecifiedgradleproject;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
 
-public class ExampleTests {
+public class AssertEqualsTests {
 
     @Test
     void testEquals() {
         assertEquals(1, notification());
-    }
-
-    @Test
-    public void testFalse() {
-        assertFalse(notification() != null && notification() > 0);
     }
 
     private Integer notification() {
