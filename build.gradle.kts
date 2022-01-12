593fe3c74316295883656de212611b667d5af1d9
Aaron Gershman <5619476+aegershman@users.noreply.github.com>
2022-01-12 15:34:02 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
     testImplementation("org.assertj:assertj-core:3.22.0")
+    testRuntimeOnly("log4j:log4j:1.+")
 }
 
 tasks.named<Test>("test") {
