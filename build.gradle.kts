0fd7eece4b2e22239fac527b2279c36837c9bd75
Aaron Gershman <aegershman@gmail.com>
2022-01-12 20:24:36 -0600

    re-eval

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,7 @@ configure<nebula.plugin.release.git.base.ReleasePluginExtension> {
 
 configure<org.owasp.dependencycheck.gradle.extension.DependencyCheckExtension> {
     format = org.owasp.dependencycheck.reporting.ReportGenerator.Format.ALL
+    // failBuildOnCVSS = 9F
 }
 
 group = "io.slugstack.oss"
@@ -36,6 +37,8 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
     testImplementation("org.assertj:assertj-core:3.22.0")
     implementation("log4j:log4j:1.2.7")
+    // testImplementation("org.apache.logging.log4j:log4j-core:2.13.0") does not appear to trigger dependency-check
+    implementation("org.apache.logging.log4j:log4j-core:2.13.0") // triggers dependency-check
     implementation("junit:junit:4.12")
 }
 
